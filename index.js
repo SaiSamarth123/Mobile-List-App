d6b3801774f20dc7ea6d85d9782b8f88eb8f5e35
SaiSamarth123 <sai003sam@gmail.com>
2023-09-07 14:15:01 -0500

    firebase database works

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL: "https://mobile-app-489eb-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
+  push(shoppingListInDB, inputValue);
+  shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
   console.log(inputValue);
 });
